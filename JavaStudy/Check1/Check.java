6b4211f2829a1496870cb165134a481aa0a8dbff
tommy24335 <a12799866@gmail.com>
2021-08-31 12:30:37 +0900

    1章チェックテスト課題　修正

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -5,30 +5,33 @@ public class Check{
       * 問1
       * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
       */
-        int[] date = {3, 1, 2, 7, 5};
+        int[] data = {3, 1, 2, 7, 5};
       /*
       * 問2
       * 以下のfor文を完成させなさい
       */
       for(int i = 0; i < 5; i++){
-          System.out.print(date[i] + " ");
+          System.out.print(data[i] + " ");
       }
       System.out.println();
-      for (int i = 0; i < 4; i++) {
-          for (int j = 4; j > i; j--) {
+      for (int i = 0; i < data.length - 1; i++) {
+        //外側のループは右端にくる大きな数字を確定させていくループ
+        //ループ変数iは0,1,2,3 = ４回ループが繰り返される
+          for (int j = data.length - 1; j > i; j--) {
+            //
               /*
               * 問3
               * 以下、配列の添字を入れてソートを完成させなさい
               */
-              if(date[j-1] > date[j]){
-                int box = date[j];
-                date[j] = date[j-1];
-                date[j-1] = box;
+              if(data[j-1] > data[j]){
+                int box = data[j];
+                data[j] = data[j-1];
+                data[j-1] = box;
               }
           }
       }
-      for(int i = 0; i < date.length; i++){
-          System.out.print(date[i] + " ");
+      for(int i = 0; i < data.length; i++){
+          System.out.print(data[i] + " ");
       }
       System.out.println();
   }
