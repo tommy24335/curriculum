b4a30ab96f19583501b133b88b70dee85a271ae6
tommy24335 <a12799866@gmail.com>
2021-09-08 07:51:13 +0900

    2-3 課題提出　修正

--- JavaStudy/2-3/src/study/Calculator.java
+++ JavaStudy/2-3/src/study/Calculator.java
@@ -2,15 +2,15 @@ package study;
 
 public class Calculator {
 
-    protected int plus(int a) {
+    protected static int plus(int a) {
         return a + 1;
     }
 
-    protected int plus(int a, int b) {
+    protected static int plus(int a, int b) {
         return a + b;
     }
 
-    protected int plus(int a, int b, int c) {
+    protected static int plus(int a, int b, int c) {
         return a + b + c;
     }
 }
