a835e1fe592a6a5cdf2c97d09d3472a3cc178d62
tommy24335 <a12799866@gmail.com>
2021-09-29 15:55:39 +0900

    2-5 提出課題　修正

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -19,7 +19,6 @@ public class Study  {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
          */
-        //Fruits info = new Fruits();
         Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04,FruitsConstants.FRUITS_COUNT_20);
                 
     }
